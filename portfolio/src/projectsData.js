02c17ef9178b6b6836a4a6db40437aa4d60d5c65
nirduk <nirdyk@gmail.com>
2022-10-31 21:19:14 +0200

    project cart appearance

--- portfolio/src/projectsData.js
+++ portfolio/src/projectsData.js
@@ -3,24 +3,24 @@ export const projectsData = [
     id: 1,
     projectName: 'Rock Paper Scissors',
     link: 'https://nirduk.github.io/rock-paper-scissors/',
-    img: 'portfolio/src/img/rockPaperScissors.png'
+    img: 'https://github.com/nirduk/nirduk.github.io/blob/main/portfolio/src/img/rockPaperScissors.png?raw=true'
   },
   {
     id: 2,
+    projectName: 'Randon Quote Machine',
+    link: 'https://nirduk.github.io/random-quote-machine/',
+    img: 'https://github.com/nirduk/nirduk.github.io/blob/main/portfolio/src/img/randomQuoteMachine.png?raw=true'
+  },
+  {
+    id: 3,
     projectName: 'Todo List',
     link: 'https://nirduk.github.io/todo-list/',
     img: 'https://github.com/nirduk/nirduk.github.io/blob/main/portfolio/src/img/todoList.png?raw=true'  
   },
   {
-    id: 3,
-    projectName: 'Randon Quote Machine',
-    link: 'https://nirduk.github.io/random-quote-machine/',
-    img: 'portfolio/src/img/randomQuoteMachine.png'
-  },
-  {
     id: 4,
     projectName: 'Simply Recipes',
     link: 'https://nirduk.github.io/simply-recipes/',
-    img: 'portfolio/src/img/simplyRecipes.png'
+    img: 'https://github.com/nirduk/nirduk.github.io/blob/main/portfolio/src/img/simplyRecipes.png?raw=true'
   }
 ]
