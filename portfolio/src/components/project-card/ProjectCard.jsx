1f8183212f43618c3ecdc49549d6b3cad1375d95
nirduk <nirdyk@gmail.com>
2022-11-15 21:50:51 +0200

    carousel functionality

--- portfolio/src/components/project-card/ProjectCard.jsx
+++ portfolio/src/components/project-card/ProjectCard.jsx
@@ -1,8 +1,10 @@
 import './projectCard.css'
 
-const ProjectCard = ({ img, link, projectName }) => {
+const ProjectCard = ({ img, link, projectName, count }) => {
+  const projectPlace = ['first', 'second', 'third', 'fourth'];
+
   return (
-    <div className="card">
+    <div className={`card ${projectPlace[count]}`}>
       <a href={link} target="_blank" rel="noreferrer">
         <p>{projectName}</p>
         <div className="card-image">
--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -2,6 +2,7 @@ import ProjectCard from '../project-card/ProjectCard'
 import './projects.css'
 import { projectsData } from '../../projectsData'
 import { useParallax } from 'react-scroll-parallax';
+import { useState } from 'react';
 
 
 const Projects = () => {
@@ -10,11 +11,25 @@ const Projects = () => {
     opacity: [0, 1, 'easeInOut']
   });
 
+  const parallaxProjects = useParallax({
+    opacity: [0, 3, 'easeInOut']
+  });
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="projects-container">
       <h3 ref={parallaxText.ref}>and these are some of my projects</h3>
-      <div className="projects">
-        <i className="fa-solid fa-chevron-left"></i>
+      <div className="projects" ref={parallaxProjects.ref}>
+        <i
+          className="fa-solid fa-chevron-left"
+          onClick={() => {
+            if(count <= 0) {
+              return count
+            }
+            setCount(count - 1)
+          }}
+        ></i>
         {projectsData.map(item => (
           <ProjectCard
             className="card"
@@ -22,9 +37,18 @@ const Projects = () => {
             link={item.link}
             projectName={item.projectName}
             key={item.id}
+            count={count}
           />
         ))}
-        <i className="fa-solid fa-chevron-right"></i>
+        <i
+          className="fa-solid fa-chevron-right"
+          onClick={() => {
+            if(count >= 3) {
+              return count
+            }
+            setCount(count + 1)
+          }}
+        ></i>
       </div>
     </div>
   )
